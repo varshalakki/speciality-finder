9a446b6eef2cffe7e8ea69304814b22544a11fe4
Varsha Lakki <varshalakki@gmail.com>
2020-05-18 21:08:28 -0700

    first commit

---
+++ src/main/java/com/cigna/assessment/specialityfinder/SpecialityFinderApplication.java
@@ -0,0 +1,13 @@
+package com.cigna.assessment.specialityfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialityFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialityFinderApplication.class, args);
+	}
+
+}
